163d5ac1acaa101357fa892ed6c6bacb30088f84
gmk <rlarkdals7@gmail.com>
2020-12-11 19:14:59 +0900

    return

--- envs/offline_env.py
+++ envs/offline_env.py
@@ -6,10 +6,11 @@ class OfflineEnv:
         self.dataloader = iter(dataloader)
         self.users_dict = users_dict
         self.data = next(self.dataloader) # {'item':items,'rating':ratings,'size':size,'userid':user_id,'idx':idx}
-        self.memory = [item[0] for itmem in self.data['item']]
-        self.user_history = self.users_dict[self.data['userid']]
+        self.memory = [item[0] for item in self.data['item']]
+        self.user_history = self.users_dict[self.data['userid'][0]]
         self.done = 0
-        self.related_movie = self.generate_related_movie(self.user_history)
+        self.related_books = self.generate_related_movie(self.user_history)
+        self.viewed_pos_books = []
     
     def generate_related_movie(self,user_history):
 
@@ -23,20 +24,29 @@ class OfflineEnv:
 
     
 
-    def step(self):
+
+
+    
+    def update_memory(self,action):
+        self.memory = list(self.memory[1:])+[action]
+    
+    def step(self,action):
             ### Env : step
-            rate = int(users_dict[int(userid_b[0])]["rating"][action])
-            try:
-                ratings = (int(rate)-3)/2
-            except:
-                ratings = 0
-            reward = torch.Tensor((ratings,))
-            ep_reward = ep_reward + ratings
+            ### 
+            rating = int(self.user_history["rating"][action])
+            normal_rating = (int(rating)-3)/2
+            reward = torch.Tensor((normal_rating,))
+            #ep_reward = ep_reward + ratings
 
             if reward > 0:
-                update_memory(memory,int(users_dict[int(userid_b[0])]["item"][action]), idx_b)
+                update_memory(action)
+                self.viewed_pos_books.append(action)
+        
+            if len(self.viewed_pos_books) == len(self.related_books):
+                done = 1
+            
+        return reward, done, self.memory
 
-            next_state = drrave_state_rep(userid_b,item_b,memory,idx_b)
-            next_state_rep = torch.reshape(next_state,[-1])
 
-        return state, reward, done
+        #next_state = drrave_state_rep(userid_b,item_b,memory,idx_b)
+        #next_state_rep = torch.reshape(next_state,[-1])
